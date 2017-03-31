136ff22378555b3e67d20d536814d2e8b1a6ad36
qifx <qifengxiu@sina.com>
2017-03-31 10:20:30 +0800

    edit for homework

--- src/main/java/com/example/MathIntegration.java
+++ src/main/java/com/example/MathIntegration.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.function.DoubleUnaryOperator;
 import java.util.function.Predicate;
 
 /**
@@ -8,31 +9,32 @@ import java.util.function.Predicate;
 public class MathIntegration {
 
   public static void main(String[] args) {
-	  
-	  Integrable i1 = x -> x;
-	  Integrable i2 = x -> x * x;
-	  Integrable i3 = x -> Math.pow(x, 3);
-	  Integrable i4 = Math::sin;
-	  
-    double d1 = integrate(i1, 10, 100, 1000);
-    System.out.println(d1);
-    double d2 = integrate(i2, 10, 100, 1000);
-    System.out.println(d2);
-    double d3 = integrate(i3, 10, 100, 1000);
-    System.out.println(d3);
-    double d4 = integrate(i4, 10, 100, 1000);
-    System.out.println(d4);
 
-    Predicate p ;
+	  DoubleUnaryOperator duo1 = x ->  x;
+	  DoubleUnaryOperator duo2 = x ->  x * x;
+	  DoubleUnaryOperator duo3 = x ->  Math.pow(x, 3);
+	  DoubleUnaryOperator duo4 = x ->  Math.sin(x);
+
+	  
+	  double d1 = integrate(duo1, 10, 100, 1000);
+	  System.out.println(d1);
+	  double d2 = integrate(duo2, 10, 100, 1000);
+	  System.out.println(d2);
+	  double d3 = integrate(duo3, 10, 100, 1000);
+	  System.out.println(d3);
+	  double d4 = integrate(duo4, 10, 100, 1000);
+	  System.out.println(d4);
+
+	  Predicate p ;
   }
 
-  public static double integrate(Integrable func, double x1, double x2, int numSlices) {
+  public static double integrate(DoubleUnaryOperator func, double x1, double x2, int numSlices) {
     if (numSlices < 1) numSlices = 1;
     double delta = (x2 - x1) / numSlices;
     double start = x1 + delta / 2.0;
     double sum = 0.0;
     for (int i = 0; i <numSlices; i++) {
-      sum += delta * func.eval(start + delta * i);
+      sum += delta * func.applyAsDouble(start + delta * i);
     }
     return sum;
   }
